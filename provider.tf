d15b9b19bcce9a1879767521713877c441c423d3
z9612 <50051891+z9612@users.noreply.github.com>
2024-09-24 17:23:25 +0900

    Create provider.tf

---
+++ provider.tf
@@ -0,0 +1,19 @@
+# Define required providers
+terraform {
+required_version = ">= 1.0.0"
+  required_providers {
+    nhncloud = {
+      source  = "nhn-cloud/nhncloud"
+      version = "1.0.2"
+    }
+  }
+}
+
+# Configure the nhncloud Provider
+provider "nhncloud" {
+  user_name   = "giun@netsoultech.com"
+  tenant_id   = "b2478be0b666477d9124b983c57cdae1"
+  password    = "1234"
+  auth_url    = "https://api-identity-infrastructure.nhncloudservice.com/v2.0"
+  region      = "KR1"
+}
